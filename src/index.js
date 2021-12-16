1261394b16e8305ea4cbddf13166df4c2cab5cfa
vicmaburrito <vmaldaraca@gmail.com>
2021-12-16 14:50:44 -0600

    Create call between webpack and API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
+const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/';
+const call = () => {
+  fetch(
+    API_URL,
+  )
+    .then((response) => response.json())
+    .then((data) => {
+    });
+};
+
+call();
